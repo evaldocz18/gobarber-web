67407443d7b4c105917988510cef3a3eeaed5e18
Evaldo <evaldocz18@gmail.com>
2020-09-07 15:48:30 -0300

    Routes corrigido

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -26,7 +26,7 @@ export default function RouteWrapper({
   return (
     <Route
       {...rest}
-      render={props => (
+      render={(props) => (
         <Layout>
           <Component {...props} />
         </Layout>
